7eb2bb033b06e82a53c0dd9af880675bda6910aa
William-SWS <samuelwill.ds@gmail.com>
2026-01-27 13:03:54 -0300

    feat: adicionando arquivo de exemplo .env e permitindo configuração do diretório de dados via variáveis de ambiente

--- mqtt_under_attack/config.py
+++ mqtt_under_attack/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from os import getenv
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -6,11 +7,14 @@ from loguru import logger
 # Load environment variables from .env file if it exists
 load_dotenv()
 
-# Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
-DATA_DIR = PROJ_ROOT / "data"
+# Base data directory (can be overridden via .env)
+_default_data_dir = PROJ_ROOT / "data"
+DATA_DIR = Path(getenv("DATA_DIR", str(_default_data_dir)))
+logger.info(f"DATA_DIR path is: {DATA_DIR}")
+
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
